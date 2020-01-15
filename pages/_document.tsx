c0ce5e9a6f3892bbd858121b9e2e3f8bee4fd95f
Jeroen Reumkens <hello@jeroenreumkens.nl>
2020-01-15 15:10:14 +0100

    Added Google Analytics and favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
-import Document from 'next/document';
+import Document, { Main, NextScript, Html } from 'next/document';
 import { ServerStyleSheet } from '../theme';
+import Head from 'next/head';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,25 @@ const Home = () => (
         href="https://fonts.googleapis.com/css?family=Merriweather:300,400,900&display=swap"
         rel="stylesheet"
       />
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>Jeroen Reumkens | Freelance frontend developer</title>
+      <link rel="shorcut/icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              var _gaq = _gaq || [];
+              _gaq.push(['_setAccount', 'UA-25954452-1']);
+              _gaq.push(['_trackPageview']);
+
+              (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+              })();
+          `
+        }}
+      />
     </Head>
     <Intro available />
     <LetsWorkTogether />

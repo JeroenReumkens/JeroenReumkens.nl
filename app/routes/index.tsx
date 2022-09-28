fa34df519d6e2057d82bb0e77c024cf516d2a8d2
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-09-28 11:33:05 +0200

    Moved sections to a folder per page.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,22 +1,20 @@
 import { Container } from '~/components/container';
 import { Footer } from '~/components/footer';
-import { DivingIntoFrontend } from '~/sections/diving-into-frontend';
-import { AllThePossibilities } from '~/sections/all-the-possibilities';
-import { Early2000s } from '~/sections/early-2000s';
-import { Intro } from '~/sections/intro';
-import { OldSkoolWebsite } from '~/sections/old-skool-website';
-import { PastDecade } from '~/sections/past-decade';
-import { SharingMyPassion } from '~/sections/sharing-my-passion';
-import { ReadOn } from '~/sections/read-on';
+import { DivingIntoFrontend } from '~/sections/home/diving-into-frontend';
+import { AllThePossibilities } from '~/sections/home/all-the-possibilities';
+import { Early2000s } from '~/sections/home/early-2000s';
+import { Intro } from '~/sections/home/intro';
+import { OldSkoolWebsite } from '~/sections/home/old-skool-website';
+import { PastDecade } from '~/sections/home/past-decade';
+import { SharingMyPassion } from '~/sections/home/sharing-my-passion';
+import { ReadOn } from '~/sections/home/read-on';
 
 export default function Index() {
   return (
     <main className="[--responsive-massive-radius:60px] sm:[--responsive-massive-radius:120px]">
       <Intro />
       <Early2000s />
-      <div
-        className="relative overflow-clip safari-overflow-clip"
-      >
+      <div className="safari-overflow-clip relative overflow-clip">
         <Container className="relative z-10">
           <img
             className="mx-auto -mt-6 mb-6 w-[600px] max-w-full md:ml-col"

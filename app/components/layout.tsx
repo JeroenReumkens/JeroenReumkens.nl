1fee1f23ac6409dbd7bcd98242453fb7880ee94c
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-09-30 16:01:11 +0200

    Added a temporary desktop navigation so I can navigate easier, until I know what the nav should look like :)

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,3 +1,5 @@
+import { Container } from './container';
+import { CtaButton } from './cta-button';
 import { Footer } from './footer';
 
 interface LayoutProps {
@@ -6,6 +8,22 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <main className="font-serif leading-tight [--responsive-massive-radius:60px] sm:[--responsive-massive-radius:120px]">
+      <header className="fixed top-[0] left-[0] z-10 hidden w-full text-body md:block">
+        <Container className="flex space-x-3 py-3 [&_a]:bg-white">
+          <CtaButton to="/" variant="small" className="ml-auto">
+            Home
+          </CtaButton>
+          <CtaButton to="/mentoring" variant="small">
+            Mentorship
+          </CtaButton>
+          <CtaButton to="/" variant="small">
+            About me
+          </CtaButton>
+          <CtaButton to="/" variant="small">
+            My vision on frontend
+          </CtaButton>
+        </Container>
+      </header>
       {children}
       <Footer />
     </main>

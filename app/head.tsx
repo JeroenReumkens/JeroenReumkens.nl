8c3fe854d552e8704053ce05962c7cf41dc4c205
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-11-04 14:53:54 +0100

    Fixed meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ const Head = () => (
     <title>Jeroen Reumkens — Frontend mentor</title>
     <meta
       name="description"
-      content="Jeroen is a passionate frontend engineer who loves to help others become beter frontenders too."
+      content="Jeroen is a passionate frontend engineer who loves to teach people the craft in frontend."
     />
     <link rel="preload" href="/img/planets.jpeg" as="image" />
     <link rel="preload" href="/img/star-twelve.svg" as="image" />
